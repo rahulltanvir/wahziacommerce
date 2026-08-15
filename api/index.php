d4f0c28d746387811a6b290052f3d53c8a535469
rahulltanvir <tanvirrahul00000@gmail.com>
2026-08-16 00:55:22 +0600

    Fix Laravel storage path on Vercel

--- api/index.php
+++ api/index.php
@@ -1,9 +1,24 @@
 <?php
 
-use Illuminate\Http\Request;
-
 require __DIR__ . '/../vendor/autoload.php';
 
 $app = require_once __DIR__ . '/../bootstrap/app.php';
+
+$app->useStoragePath('/tmp/storage');
+
+$directories = [
+    '/tmp/storage/framework/cache',
+    '/tmp/storage/framework/sessions',
+    '/tmp/storage/framework/views',
+    '/tmp/storage/logs',
+];
+
+foreach ($directories as $directory) {
+    if (!is_dir($directory)) {
+        mkdir($directory, 0777, true);
+    }
+}
+
+use Illuminate\Http\Request;
 
 $app->handleRequest(Request::capture());
