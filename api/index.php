7ed2cbea6d9bfd2b5acc27d836b85b058956c590
rahulltanvir <tanvirrahul00000@gmail.com>
2026-08-16 00:01:48 +0600

    Fix Vercel Laravel entry point

--- api/index.php
+++ api/index.php
@@ -1,9 +1,9 @@
 <?php
 
+use Illuminate\Http\Request;
+
 require __DIR__ . '/../vendor/autoload.php';
 
 $app = require_once __DIR__ . '/../bootstrap/app.php';
-
-use Illuminate\Http\Request;
 
 $app->handleRequest(Request::capture());
