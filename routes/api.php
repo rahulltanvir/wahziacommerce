7d0511c57779b3fcc14f7a3b48b80be77187b4bf
rahulltanvir <tanvirrahul00000@gmail.com>
2026-09-12 00:19:02 +0600

    problem sovle

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,27 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\Product;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+Route::get('/products', function () {
+    return Product::select(
+        'id',
+        'category_id',
+        'subcategory_id',
+        'brand_id',
+        'name',
+        'slug',
+        'sku',
+        'product_code',
+        'stock',
+        'regular_price',
+        'sale_price',
+        'discount',
+        'thumbnail',
+        'featured',
+        'status'
+    )->get();
+});
